658c36506d89f9e3e4e9ca268ceb4acfeb89da53
andersonda <andersonda8109@gmail.com>
2023-08-05 09:51:04 -0700

    extracted frequencies to separate configuration file

---
+++ config.js
@@ -0,0 +1,6 @@
+let config = {
+  "use-custom-frequencies" : false,
+  "default-frequencies" : {"500": 500, "501": 501, "502": 502, "503": 503, "504": 504,
+    "A": 499, "B": 501.5, "C": 506, "D": 509},
+  "custom-frequencies" : {}
+}
